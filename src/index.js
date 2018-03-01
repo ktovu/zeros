03c2267a061d6cfd9e301045b5c6f42c9a9dcf8c
ktovu <andrew.konash@yandex.ru>
2018-03-02 02:15:57 +0300

    Interesting task ))

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var result = 0;
+
+  do{
+      var a = Math.floor(number/5);
+      number = a;
+      result += a;
+  }while(a >= 5);
+
+  return(result);
+  
 }
